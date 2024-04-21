c87814277d3922b8107857be7085776ac822b731
thtruct <21522722@gm.uit.edu.vn>
2024-04-22 02:01:00 +0700

    FE-bookinghistory

---
+++ app/src/main/java/com/example/metro_booking_project/MyAdapter_BookingHistory.java
@@ -0,0 +1,42 @@
+package com.example.metro_booking_project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter_BookingHistory extends RecyclerView.Adapter<MyViewHolder_BookingHistory> {
+
+    public MyAdapter_BookingHistory(Context context, List<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    Context context;
+    List<Item> items;
+    @NonNull
+    @Override
+    public MyViewHolder_BookingHistory onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder_BookingHistory(LayoutInflater.from(context).inflate(R.layout.item_view_bookinghistory, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder_BookingHistory holder, int position) {
+        holder.nameView.setText(items.get(position).getName());
+        holder.positionFromView.setText(items.get(position).getName());
+        holder.positionToView.setText(items.get(position).getName());
+        holder.timeView.setText(items.get(position).getName());
+        holder.dateView.setText(items.get(position).getName());
+        holder.imageView.setImageResource(items.get(position).getImage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
