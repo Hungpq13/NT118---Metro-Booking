7e938013f9e63a1ebecd9e4d9ba33dfbb7ec6349
thtruct <21522722@gm.uit.edu.vn>
2024-05-05 14:56:49 +0700

    FE-personal-info

---
+++ app/src/main/java/com/example/metro_booking_project/Personal_Information.java
@@ -0,0 +1,38 @@
+package com.example.metro_booking_project;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+
+
+
+
+
+public class Personal_Information extends AppCompatActivity {
+    ImageView avatarImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_personal_information);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void onEditAvatarClickd(View view){
+        openImagePicker();
+    }
+    private void openImagePicker(){
+
+    }
+
+}
