bdd60b99e7f4a87606dc5790442214c2ff2d8fbb
Hungpq13 <146099528+Hungpq13@users.noreply.github.com>
2024-06-17 19:02:56 +0700

    update main

---
+++ app/src/main/java/com/example/nt118project/MainFunction/Main_payment.java
@@ -0,0 +1,87 @@
+package com.example.nt118project.MainFunction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.nt118project.R;
+import com.example.nt118project.bottomnav.MenuActivity;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class Main_payment extends AppCompatActivity {
+    private Spinner Loaive;
+    private Spinner sThanhtoan;
+    private TextView Giatien;
+    ImageView back ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_paymentt);
+
+        Loaive = findViewById(R.id.sLoaive);
+        sThanhtoan = findViewById(R.id.sThanhtoan);
+        Giatien = findViewById(R.id.Giatien);
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main_payment.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+        ArrayList<String> arrayLoaiVe = new ArrayList<>();
+        arrayLoaiVe.add("Chọn loại vé");
+        arrayLoaiVe.add("Vé 1 chiều");
+        arrayLoaiVe.add("Vé 2 chiều");
+
+        ArrayList<String> arraysThanhtoan = new ArrayList<>();
+        arraysThanhtoan.add("Thanh toán momo");
+        arraysThanhtoan.add("Thanh toán ngân hàng");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayLoaiVe);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Loaive.setAdapter(arrayAdapter);
+
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arraysThanhtoan);
+        arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sThanhtoan.setAdapter(arrayAdapter1);
+
+        Loaive.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                if (selectedItem.equals("Vé 1 chiều")) {
+                    Giatien.setText("20.000 VND");
+                } else if (selectedItem.equals("Vé 2 chiều")) {
+                    Giatien.setText("40.000 VND");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/nt118project/bottomnav/MenuActivity.java
+++ app/src/main/java/com/example/nt118project/bottomnav/MenuActivity.java
@@ -1,38 +1,74 @@
 package com.example.nt118project.bottomnav;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.nt118project.AdminSystem.AdminActivity;
 import com.example.nt118project.BookingHistory.BookingHistory;
+import com.example.nt118project.MainFunction.Main_payment;
 import com.example.nt118project.MainFunction.MapsActivity;
 import com.example.nt118project.MainFunction.NearStation1Activity;
 import com.example.nt118project.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import java.io.IOException;
+import java.util.List;
 
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     Button metro ;
     Button Search ;
+    Button Payment ;
     TextView usernameTextView;
     ImageView avatarImageView;
+    private GoogleMap mMap;
+    private SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
+        Payment = findViewById(R.id.payment);
         metro = findViewById(R.id.metro);
         Search = findViewById(R.id.search);
         avatarImageView = findViewById(R.id.avatarImageView);
         usernameTextView = findViewById(R.id.usernameTextView);
+
+
+
+        Payment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, Main_payment.class);
+                startActivity(intent);
+            }
+        });
         avatarImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,7 +97,73 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        searchView = findViewById(R.id.searchView);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        // Make the SearchView auto-open
+        searchView.setIconifiedByDefault(false);
+        searchView.setIconified(false);
+        searchView.requestFocusFromTouch();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if (location != null || !location.equals("")) {
+                    Geocoder geocoder = new Geocoder(MenuActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location,1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (addressList != null && addressList.size() > 0) {
+                        Address address = addressList.get(0);
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                    } else {
+                        Toast.makeText(MenuActivity.this, "Vị trí không hợp lệ !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Default location
+        LatLng defaultLocation = new LatLng(10.8700, 106.8032);
+        mMap.addMarker(new MarkerOptions().position(defaultLocation).title("you").icon(BitmapDescriptorFactory.fromResource(R.drawable.markeruser)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLocation,15));
+    }
+
+    private BitmapDescriptor bitmapDescriptor(Context context, int vectorResid)
+    {
+        Drawable VectorDrawable = ContextCompat.getDrawable(context , vectorResid);
+        VectorDrawable.setBounds(0 ,0,VectorDrawable.getIntrinsicWidth(),VectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(VectorDrawable.getIntrinsicWidth(),VectorDrawable.getIntrinsicHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        VectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
 
     }
 
-}
+
+
+
+
