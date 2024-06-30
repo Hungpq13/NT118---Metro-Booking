a5feef028da01809eade1d6d21242c528a09eb41
thtruct <21522722@gm.uit.edu.vn>
2024-06-30 22:32:53 +0700

    edit&fix-UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,4 @@ dependencies {
     implementation("com.google.firebase:firebase-storage:21.0.0")
     implementation("com.squareup.picasso:picasso:2.71828")
     implementation("com.squareup.okhttp3:okhttp:4.9.2")
-
 }
